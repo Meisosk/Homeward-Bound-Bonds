24933a3dc2eb066b18893729db89847a02a198eb
Meir Soskil <meirsoskil@gmail.com>
2023-08-28 17:22:01 -0400

    redid the routes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express()
 const port = 3000
 
 app.engine('html', es6);
+app.set('views', 'views');
 app.set('view engine', 'html');
 app.use(express.static('public'))
 
@@ -15,11 +16,36 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
+    res.send("hello world")
     res.render("desk")
 })
 
-app.get('/example', (req, res) => {
+app.get('/signup', (req, res) => {
     res.render("example")
+})
+
+app.get('/signin', (req, res) => {
+    res.render("sign-in")
+})
+
+app.get('/petprofile', (req, res) => {
+    res.render("pet-profile")
+})
+
+app.get('/contact', (req, res) => {
+    res.render("exampleContact")
+})
+
+app.get('/profile', (req, res) => {
+    res.render("profile")
+})
+
+app.get('/rehome', (req, res) => {
+    res.render("re-home")
+})
+
+app.get('/adopted', (req, res) => {
+    res.render("recently-adopted")
 })
 
 app.listen(port, () => {
