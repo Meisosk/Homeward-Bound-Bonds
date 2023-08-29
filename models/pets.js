813806038d2e8646f7f2ca8d0be3f3daed1d90ea
Aleytys Isbell <isbellaleytys@gmail.com>
2023-08-29 10:32:52 -0400

    tables and migrations step1

---
+++ models/pets.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Pets extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Pets.init({
+    name: DataTypes.STRING,
+    pics: DataTypes.BLOB,
+    age: DataTypes.STRING,
+    gender: DataTypes.STRING,
+    weight: DataTypes.INTEGER,
+    type: DataTypes.STRING,
+    bio: DataTypes.TEXT,
+    isAdopted: DataTypes.BOOLEAN,
+    ownerId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Pets',
+  });
+  return Pets;
+};
